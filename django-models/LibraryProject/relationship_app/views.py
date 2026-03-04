d0de7f0dae51c8e50ca52c175b1a47cdfdb8dfb4
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-03-04 07:12:38 -0800

    Added Views and Urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.views.generic import DetailView
+
+from .models import Book, Library
+
+
+def list_books(request):
+    """
+    Function-based view:
+    Lists all books stored in the database.
+    """
+    books = Book.objects.select_related("author").all()
+    return render(request, "list_books.html", {"books": books})
+
+
+class LibraryDetailView(DetailView):
+    """
+    Class-based view:
+    Displays details for a specific library and lists all books in that library.
+    """
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Prefetch books + authors to reduce queries
+        context["library"] = Library.objects.prefetch_related("books__author").get(pk=self.object.pk)
+        return context
