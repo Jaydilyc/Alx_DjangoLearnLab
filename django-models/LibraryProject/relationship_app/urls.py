36bbf8d2b27813a4a0e8e39246c6bb61bd6e3996
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-03-05 12:00:35 -0800

    Added Custom Permissions +1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("member-role/", views.member_view, name="member_view"),
 
     # Secured book actions (Task 4)
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
 ]
