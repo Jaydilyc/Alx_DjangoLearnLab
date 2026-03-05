a2bcdca216274c49b37ac95e7bd8aca4b8ec2a47
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-03-05 11:19:22 -0800

    Imported .view in Relationship Urls.py +1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
-    # Existing routes
-    path("books/", list_books, name="list_books"),
-    path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("books/", views.list_books, name="list_books"),
+    path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    # Authentication routes
+    # Authentication URLs
     path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),
 ]
