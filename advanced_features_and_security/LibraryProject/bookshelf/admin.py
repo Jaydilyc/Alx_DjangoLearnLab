89f98a2e68ba2c7cba12dd88de2568b899c2b6f1
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-03-06 00:37:13 -0800

    Added Custom User Model afas +2

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from .models import Book
+from .models import CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -13,3 +15,23 @@ class BookAdmin(admin.ModelAdmin):
 
     # 3) Add a search box (search by title or author)
     search_fields = ("title", "author")
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # show extra fields in admin list
+    list_display = ("username", "email", "date_of_birth", "is_staff")
+
+    # add custom fields to the user edit page
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    # add custom fields to the user creation page
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
