c464582b8a430492c2025f6b9c6cda950a4df657
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-03-07 15:46:48 -0800

    Added Security Feature +1

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,19 +2,21 @@ from django import forms
 from .models import Book
 
 
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200)
+    author = forms.CharField(max_length=100)
+    publication_year = forms.IntegerField()
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+
+        if len(title.strip()) < 2:
+            raise forms.ValidationError("Title must be at least 2 characters long.")
+
+        return title
+
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ["title", "author", "publication_year"]
-
-    def clean_title(self):
-        title = self.cleaned_data["title"]
-        if len(title.strip()) < 2:
-            raise forms.ValidationError("Title must be at least 2 characters long.")
-        return title
-
-    def clean_author(self):
-        author = self.cleaned_data["author"]
-        if len(author.strip()) < 2:
-            raise forms.ValidationError("Author name must be at least 2 characters long.")
-        return author
