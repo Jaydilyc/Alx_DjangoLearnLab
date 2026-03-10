60065ed80aab2ef8dd8d8a343f501c5201de707d
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-03-10 13:41:22 -0700

    Fix comment creation URL pattern

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,6 +7,7 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    CommentCreateView,
     CommentUpdateView,
     CommentDeleteView,
 )
@@ -25,7 +26,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    path('post/<int:pk>/comment/new/', views.add_comment, name='add-comment'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
