4102292cd06aeda049c49437733d0a1c994045dd
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-03-10 13:38:20 -0700

    Add comment functionality with create, edit, and delete features +1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -106,6 +106,18 @@ def add_comment(request, pk):
     return redirect('post-detail', pk=post.pk)
 
 
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
